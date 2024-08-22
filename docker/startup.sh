f277b327f2f06d722f8fafd0ebee6e0af4d62836
Philipp Ost <philipp.ost@kit.edu>
2024-08-22 18:16:16 +0200

    Adapt bind mounts, do not create certain directories

--- docker/startup.sh
+++ docker/startup.sh
@@ -2,17 +2,9 @@
 
 # Licensed under the terms of the MIT License.
 
-# Create required directories if missing
-for dir in backup data logs
-do
-	mkdir -p docker/$dir 2>/dev/null || true
-done
-
 docker run -d -it -p 3030:3030 \
 	--name DCENG \
-	--mount type=bind,source="$(pwd)"/docker/backup,target=/fuseki-base/backup \
-	--mount type=bind,source="$(pwd)"/docker/config,target=/fuseki-base/configuration \
-	--mount type=bind,source="$(pwd)"/docker/data,target=/fuseki-base/databases \
-	--mount type=bind,source="$(pwd)"/docker/logs,target=/fuseki-base/logs \
+	--mount type=bind,source="$(pwd)"/databases,target=/fuseki/databases \
+	--mount type=bind,source="$(pwd)"/logs,target=/fuseki/logs \
 	--restart unless-stopped \
 	dceng:v1
