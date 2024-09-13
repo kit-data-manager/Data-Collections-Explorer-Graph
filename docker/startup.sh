23f17a20bdbdad605c71bdc06a67f4afe61b3c89
Philipp Ost <philipp.ost@kit.edu>
2024-09-13 18:33:02 +0200

    Handle old container - check if old container is running, stop it, remove it - start new container While here, make the script more portable

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,7 +1,17 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 # Licensed under the terms of the MIT License.
 
+# Check whether an old dceng-based container is still around and get its ID
+CONTAINER=$(docker container ls -a | awk '$2 ~ /^dceng$/ { print $1 }')
+
+# Stop old running container and remove it
+if [ $CONTAINER ]; then
+	docker container stop $CONTAINER;
+	docker container rm $CONTAINER;
+fi
+
+# Create a new container
 docker run -d -it -p 3030:3030 \
 	--name DCENG \
 	--restart unless-stopped \
