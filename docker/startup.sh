c000fe9144f2039e025ace325513853f6332390b
Philipp Ost <philipp.ost@kit.edu>
2024-09-20 13:43:27 +0200

    Revert "Fix database location to point Docker and Fuseki into the right direction"
    This reverts commit dcced9e501867cee7b348a47a344a4dc76a6cdd4.

--- docker/startup.sh
+++ docker/startup.sh
@@ -18,4 +18,4 @@ docker run -d -it -p 3030:3030 \
 	--mount type=bind,source="$(pwd)"/databases,target=/fuseki/databases \
 	--mount type=bind,source="$(pwd)"/logs,target=/fuseki/logs \
 	-t dceng \
-	--tdb2 --loc databases /dce
+	--tdb2 --loc databases/DB2 /dce
